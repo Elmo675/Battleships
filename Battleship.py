49fa198c317095e6545b97b57c7d3e36e61aa4af
Elmo675 <40466868+Elmo675@users.noreply.github.com>
2020-07-16 22:45:37 +0200

    Added message and legend in game, soon 1st game with PC🚀

--- Battleship.py
+++ Battleship.py
@@ -1,31 +1,36 @@
 import pygame
-
-
-
-#moving function
-def move(player_side,enemy_side,x,y):
-    pass
-
+import time
 pygame.init()
- 
+## VARIABLES
+
+# Colors rgb
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 blue = (176,224,230)
+green = (24,110,90)
 
-#Size of rectangle
+# Size of rectangle
 WIDTH = 30
 
-#Margin 
+# Margin 
 MARGIN = 2
 
-#diffrence
+# Diffrence
 DIFFRENCE = WIDTH-MARGIN
 
-#Size of game field
-SIZE = 8
+# Size of game field
+SIZE = 10
 
-# Initialize player and enemy side 
+# Exit module 
+game_over = False
+
+#Polish alphabet
+ALPHABET = "ABCDEFGHIJKLMNOPRSTUWYZ"
+
+##INITIALIZING
+
+# Initialize player and enemy side
 player_side = []
 enemy_side  = []
 for row in range(SIZE):
@@ -36,15 +41,50 @@ for row in range(SIZE):
     for column in range(SIZE):
         player_side[row].append(0)
         enemy_side[row].append(0)
-dis = pygame.display.set_mode((SIZE*80, SIZE*50))
+# Initialize display
+dis = pygame.display.set_mode((SIZE*90, SIZE*50))
 pygame.display.set_caption('Battleship')
- 
-game_over = False
- 
+# Initialize font style
+mesg_style = pygame.font.SysFont(None, 50)
+legend_style = pygame.font.SysFont("comicsansms", 15)
+
 x = 0
 y = 0
 clock = pygame.time.Clock()
- 
+
+
+## FUNCTION
+
+# Moving function
+def move(player_side,enemy_side,x,y):
+    pass
+
+# Message function 
+def message(msg,color):
+    mesg = mesg_style.render(msg, True, color)
+    dis.blit(mesg, [SIZE*35, SIZE*40])
+    pygame.display.update()
+    time.sleep(1)
+
+def legend(side):
+    if side==0: #for the left table
+        delta=0
+    else:       #for the right table moved with delta
+        delta=WIDTH*(SIZE+2)
+    text="     ".join(ALPHABET[:SIZE:]) #take exacly amount of Alphabet letters
+    legend_top = legend_style.render(text,True,black)
+    dis.blit(legend_top, [WIDTH*2+delta, WIDTH])
+    for number in range(SIZE):
+        legend_left=legend_style.render(str(number+1),True,black)
+        dis.blit(legend_left,[WIDTH+delta,WIDTH*number+2*WIDTH])
+    
+## TEST SCENARIOS
+#player_side[1][6] = 2
+#player_side[1][5] = 1
+
+
+##MAIN LOOP
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -52,23 +92,14 @@ while not game_over:
         if event.type == pygame.MOUSEBUTTONDOWN:
             x,y=pygame.mouse.get_pos()
         if event.type == pygame.KEYDOWN:
-            if event.key == K_SPACE:
+            if event.key == pygame.K_SPACE:
                 move(player_side,enemy_side,x,y)
-
-    #fill background white
+                message("HIT",red)
+    # Fill background white
     dis.fill(white)
 
-    #player side drawning
-    for row in range(SIZE):
-        for column in range(SIZE):
-            color = blue
-            if player_side[row][column]   == 1:
-                color = black
-            elif player_side[row][column] == 2:
-                color = red
-            pygame.draw.rect(dis, color, [(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])  
-
-    #enemy side drawning
+    # Enemy side drawning
+    legend(0)
     for row in range(SIZE):
         for column in range(SIZE):
             color = blue
@@ -76,16 +107,30 @@ while not game_over:
                 color = black
             elif enemy_side[row][column] == 2:
                 color = red
+            pygame.draw.rect(dis, color, [(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])  
+
+    # Player side drawning
+    legend(1)
+    for row in range(SIZE):
+        for column in range(SIZE):
+            color = blue
+            if player_side[row][column]   == 1:
+                color = black
+            elif player_side[row][column] == 2:
+                color = red
             pygame.draw.rect(dis, color, [(row+SIZE+4)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])
 
-    #drawning choosing rectangle
+    # Drawning choosing rectangle
     for row in range(SIZE):
         for column in range(SIZE):
             if (row+2)*WIDTH <=x and ((row+2)*WIDTH+DIFFRENCE) >=x and (column+2)*WIDTH <=y and ((column+2)*WIDTH+DIFFRENCE) >=y:
-                pygame.draw.rect(dis,black,[(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])
+                pygame.draw.rect(dis,green,[(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])
 
-    #update screen 
+    # Update screen 
     pygame.display.update()
     clock.tick(30)
+
+    
+#close game and quit program
 pygame.quit()
 quit()
