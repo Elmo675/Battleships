a1a456fd36ad490b2a07af29beceb7e0620c9761
Elmo675 <40466868+Elmo675@users.noreply.github.com>
2020-07-20 01:51:33 +0200

    HOTFIX
    Many small bug fixes:
    Enemy dont move after bad player move
    there is some comunicate if we shoot the same field we missed
    cant choose space beetween rectangles (for example margin beetween A1 and A2)
    better ship settlement ( less iterations to find a correct place to set the ship)
    better test scenarios (can set ships after fail  for example affter 500 trys still can choose ship settlement, dont have to do it in 1 iteration)
    focus on field dissapear affter a move( mouse click position is reseted)
    changed dark_blue into yellow (bad variable name)

--- Battleship.py
+++ Battleship.py
@@ -10,7 +10,7 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 blue = (176,224,230)
 green = (24,110,90)
-dark_blue = (255,255,51)
+yellow = (255,255,51)
 
 # Size of rectangle
 WIDTH = 30
@@ -22,10 +22,10 @@ MARGIN = 2
 DIFFRENCE = WIDTH-MARGIN
 
 # Size of game field
-SIZE = 15
+SIZE = 10
 
 # Game xpeed
-SPEED = 1
+SPEED = 0.3
 
 # Exit module 
 game_over = False
@@ -46,9 +46,11 @@ for row in range(SIZE):
     for column in range(SIZE):
         player_side[row].append(0)
         enemy_side[row].append(0)
+
 # Initialize display
 dis = pygame.display.set_mode((SIZE*90, SIZE*50))
 pygame.display.set_caption('Battleship')
+
 # Initialize font style
 mesg_style   = pygame.font.SysFont(None, 50)
 legend_style = pygame.font.SysFont("comicsansms", 15)
@@ -69,24 +71,24 @@ def move(player_side,enemy_side,x,y):
     shotY = -1
     for row in range(SIZE):
         for column in range(SIZE):
-            if (row+2)*WIDTH <=x and ((row+2)*WIDTH+DIFFRENCE) >=x and (column+2)*WIDTH <=y and ((column+2)*WIDTH+DIFFRENCE) >=y:
+            if (row+2)*WIDTH <x and (row+3)*WIDTH >=x and (column+2)*WIDTH <y and (column+3)*WIDTH >=y:
                 shotX=row
                 shotY=column
                 break
     if shotX == -1 or shotY == -1:
-        message("select a field to shoot")
-        return
+        message("SELECT A FIELD TO SHOOT")
+        return False
     if enemy_side[shotX][shotY] == 1:
         enemy_side[shotX][shotY] = 2
         message("HIT")
-        return
-    elif enemy_side[shotX][shotY] == 2:
-        message("YOS SHOT THIS FIELD BEFORE, TRY HARDER")
-        return
+        return True
+    elif enemy_side[shotX][shotY] == 2 or enemy_side[shotX][shotY] == 3 :
+        message("YOU SHOOT THIS FIELD BEFORE")
+        return True
     elif enemy_side[shotX][shotY] == 0:
         enemy_side[shotX][shotY] = 3
         message("MISS")
-        return
+        return True
 
 # AI
     
@@ -95,18 +97,18 @@ def beginer_PC(player_side,enemy_side):
     beginer_y = random.randrange(SIZE -1)
     if beginer_x == -1 or beginer_y == -1:
         message("ERROR")
-        return
+        return False
     if player_side[beginer_x][beginer_y] == 1:
         player_side[beginer_x][beginer_y] = 2
         message("YOU HAVE BEEN HITED")
-        return
-    elif player_side[beginer_x][beginer_y] == 2:
+        return True
+    elif player_side[beginer_x][beginer_y] == 2 or player_side[beginer_x][beginer_y] == 3:
         message("ENEMY MISSED STUPIDLY")
-        return
+        return True
     elif player_side[beginer_x][beginer_y] == 0:
         player_side[beginer_x][beginer_y] = 3
         message("ENEMY MISS")
-        return
+        return True
 
 
 
@@ -117,7 +119,7 @@ def beginer_PC(player_side,enemy_side):
 def message(msg):
     dis.fill(white)
     mesg = mesg_style.render(msg, True, red)
-    dis.blit(mesg, [SIZE*35, SIZE*40])
+    dis.blit(mesg, [SIZE*30, SIZE*40])
     pygame.display.update()
     time.sleep(SPEED)
     
@@ -160,37 +162,39 @@ def random_ship_settlement(table,ship_table):
             row       = random.randrange(SIZE -1) # starting position 
             column    = random.randrange(SIZE -1)
             direction = random.randint(0,3)
-            if row - position <0 or row + position >= SIZE  or column - position < 0 or column + position >= SIZE: # out of board cant add ship 
-                counter +=1
-                continue
+            
             if table[row][column] == 1: # there is a ship in this coordinates
                 counter +=1
                 continue
-
-            if table[row][column] == 0 and position == 0 :
-                if not is_ship_allowed(row,column,position+1,direction,table) : # stick to other ship cant add a new ship
-                    counter +=1
-                    continue
-                else:
-                    table[row][column] = 1
-                    counter = 0 #ship added, counter reseted
-                    ship_table[position] -= 1# success
+            
+            if   direction == 0 and row - position < 0: #ship out of board cant be set
+                counter +=1
+                continue 
+            elif direction == 1 and column + position >= SIZE: #ship out of board cant be set
+                counter +=1
+                continue
+            elif direction == 2 and row + position >= SIZE: #ship out of board cant be set
+                counter +=1
+                continue
+            elif direction == 3 and column - position < 0: #ship out of board cant be set
+                counter +=1
+                continue
+                            
+            if not is_ship_allowed(row,column,position+1,direction,table): # stick to other ship cant add a new ship
+                counter +=1
+                continue
             else:
-                if not is_ship_allowed(row,column,position+1,direction,table): # stick to other ship cant add a new ship
-                    counter +=1
-                    continue
-                else:
-                    for i in range(position+1): # add  ship
-                        if direction == 0: # up
-                            table[row-i][column] = 1
-                        elif direction == 1: # right
-                            table[row][column+i] = 1
-                        elif direction == 2: # down
-                            table[row+i][column] = 1
-                        elif direction == 3: # left
-                            table[row][column-i] = 1
-                    counter = 0
-                    ship_table[position] -= 1# success
+                for i in range(position+1): # add  ship
+                    if direction == 0: # up
+                        table[row-i][column] = 1
+                    elif direction == 1: # right
+                        table[row][column+i] = 1
+                    elif direction == 2: # down
+                        table[row+i][column] = 1
+                    elif direction == 3: # left
+                        table[row][column-i] = 1
+                counter = 0
+                ship_table[position] -= 1# success
 
 
 def is_ship_allowed(row,column,lenght,direction,table):
@@ -246,25 +250,29 @@ def is_ship_allowed(row,column,lenght,direction,table):
 
 
 end = False
-backup = player_side
-backup2 = enemy_side
 counter = 0
 while not end:
     try:
-        player_side = random_ship_settlement(player_side,[0,4,3,2,1,1,0])
-        enemy_side  = random_ship_settlement(enemy_side,[0,4,3,2,1,1,0])
+        player_side = random_ship_settlement(player_side,[0,0,0,0,0,0,5])
+        enemy_side  = random_ship_settlement(enemy_side,[0,0,0,0,0,0,5])
     except IndexError :
         pass
-    if player_side[0][0]==-1:
-        print("not good")
-        player_side = backup
-    elif enemy_side[0][0]==-1:
-        print("not good enemy")
-        enemy_side = backup2
+    if player_side[0][0] == -1 or enemy_side[0][0] == -1: #fail, initialize again
+        player_side = []
+        enemy_side  = []
+        print("NOT GOOD")
+        for row in range(SIZE):
+            # Add an empty array that will hold each cell
+            # in this row
+            player_side.append([])
+            enemy_side.append([])
+            for column in range(SIZE):
+                player_side[row].append(0)
+                enemy_side[row].append(0)
     else:
         end = True
     counter +=1
-    if counter ==1000:break
+    if counter == 10000:break
 if not end:
     quit()
 
@@ -280,9 +288,15 @@ while not game_over:
             x,y=pygame.mouse.get_pos()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE: # if space is clicked then perform a move
-                move(player_side,enemy_side,x,y)
-                beginer_PC(player_side,enemy_side)
-                #message("HIT")
+                if move(player_side,enemy_side,x,y)   == False: # if player did not selected the field, do not allow computer to move
+                    continue
+                if beginer_PC(player_side,enemy_side) == False:
+                    print("i dont know what happened")
+                    break
+                #reset cursor position
+                x=0
+                y=0
+
     # Fill background white
     dis.fill(white)
 
@@ -298,7 +312,7 @@ while not game_over:
 ##                color = black
             ############
             elif enemy_side[row][column] == 3:
-                color = dark_blue
+                color = yellow
             pygame.draw.rect(dis, color, [(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])  
 
     # Player side drawning
@@ -311,13 +325,13 @@ while not game_over:
             elif player_side[row][column] == 2:
                 color = red
             elif player_side[row][column] == 3:
-                color = dark_blue
+                color = yellow
             pygame.draw.rect(dis, color, [(row+SIZE+4)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])
 
     # Drawning player cursor position
     for row in range(SIZE):
         for column in range(SIZE):
-            if (row+2)*WIDTH <=x and ((row+2)*WIDTH+DIFFRENCE) >=x and (column+2)*WIDTH <=y and ((column+2)*WIDTH+DIFFRENCE) >=y:
+            if (row+2)*WIDTH <x and (row+3)*WIDTH >=x and (column+2)*WIDTH <y and (column+3)*WIDTH >=y:
                 pygame.draw.rect(dis,green,[(row+2)*WIDTH,(column+2)*WIDTH,DIFFRENCE,DIFFRENCE])
                 print(row,column)
                 break
